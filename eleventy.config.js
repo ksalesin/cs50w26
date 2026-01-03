198734e14dc515add5b37fae67448a1f869cdca6
Kate <katherine.a.salesin@dartmouth.edu>
2026-01-03 17:49:42 -0500

    add cs50 to path prefix

--- eleventy.config.js
+++ eleventy.config.js
@@ -22,6 +22,6 @@ module.exports = async function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("github-markdown.css");
 
     return {
-        pathPrefix: "/~ksalesin/"
+        pathPrefix: "/~ksalesin/cs50/"
     };
 }
